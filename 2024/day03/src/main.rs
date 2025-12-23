d75c715a3f1ec453b32039bf594b558fa5e64516
Anton Amirian <extuessl@gmail.com>
2025-12-23 14:28:22 +0200

    2024: day 3 part 2

--- 2024/day03/src/main.rs
+++ 2024/day03/src/main.rs
@@ -1,13 +1,20 @@
-use std::fs::read_to_string;
 use nom::{
-    IResult,
-    Parser,
+    branch::alt,
     bytes::complete::tag,
     character::complete::digit1,
     combinator::recognize,
-    sequence::{delimited, separated_pair},
     error::{Error, ErrorKind},
+    sequence::{delimited, separated_pair},
+    IResult, Parser,
 };
+use std::fs::read_to_string;
+
+#[derive(Debug)]
+enum Calc {
+    Mul(i32, i32),
+    Do,
+    Dont,
+}
 
 fn number_1_to_3(input: &str) -> IResult<&str, i32> {
     let (rest, digits) = recognize(digit1).parse(input)?;
@@ -20,13 +27,21 @@ fn number_1_to_3(input: &str) -> IResult<&str, i32> {
     Ok((rest, value))
 }
 
-fn mul_parser(input: &str) -> IResult<&str, i32> {
+fn do_parser(input: &str) -> IResult<&str, Calc> {
+    tag("do()").map(|_| Calc::Do).parse(input)
+}
+
+fn dont_parser(input: &str) -> IResult<&str, Calc> {
+    tag("don't()").map(|_| Calc::Dont).parse(input)
+}
+
+fn mul_parser(input: &str) -> IResult<&str, Calc> {
     delimited(
         tag("mul("),
         separated_pair(number_1_to_3, tag(","), number_1_to_3),
         tag(")"),
     )
-    .map(|(a, b)| a * b)
+    .map(|(a, b)| Calc::Mul(a, b))
     .parse(input)
 }
 
@@ -38,8 +53,49 @@ pub fn solve(input: &str) -> i32 {
         let slice = &input[i..];
 
         if let Ok((rest, value)) = mul_parser(slice) {
-            sum += value;
+            match value {
+                Calc::Mul(a, b) => sum += a * b,
+                _ => (),
+            };
             i += slice.len() - rest.len();
+        } else {
+            i += 1; // skip corrupted character
+        }
+    }
+
+    sum
+}
+
+fn parse_next(input: &str) -> Option<(usize, Calc)> {
+    let mut parsers = alt((dont_parser, do_parser, mul_parser));
+
+    if let Ok((rest, instr)) = parsers.parse(input) {
+        let consumed = input.len() - rest.len();
+        Some((consumed, instr))
+    } else {
+        None
+    }
+}
+
+pub fn solve_part2(input: &str) -> i32 {
+    let mut sum = 0;
+    let mut i = 0;
+    let mut cont = true;
+
+    while i < input.len() {
+        let slice = &input[i..];
+
+        if let Some((consumed, value)) = parse_next(slice) {
+            match value {
+                Calc::Mul(a, b) => {
+                    if cont {
+                        sum += a * b
+                    }
+                }
+                Calc::Do => cont = true,
+                Calc::Dont => cont = false,
+            };
+            i += consumed;
         } else {
             i += 1; // skip corrupted character
         }
@@ -54,8 +110,10 @@ fn main() {
 
     let ans = solve(&contents.trim());
     println!("Day 3 part 1 result: {}", ans);
-}
 
+    let ans = solve_part2(&contents.trim());
+    println!("Day 3 part 2 result: {}", ans);
+}
 
 #[cfg(test)]
 mod tests {
@@ -64,9 +122,7 @@ mod tests {
     #[test]
     fn initial_part1() {
         assert_eq!(
-            solve(
-                "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-            ),
+            solve("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"),
             161
         );
     }
@@ -74,5 +130,15 @@ mod tests {
     #[test]
     fn correct_handle_of_mul() {
         assert_eq!(solve("mul(34)mul(2,3)mul((2,4))"), 6);
+    }
+
+    #[test]
+    fn initial_part2() {
+        assert_eq!(
+            solve_part2(
+                "xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+            ),
+            48
+        );
     }
 }
