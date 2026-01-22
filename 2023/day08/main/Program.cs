8dd60a92f2b9fa732ec2bb7bf3a529b14bf17118
Anton Amirian <extuessl@gmail.com>
2026-01-23 00:05:40 +0200

    aoc 2023 day 8 part 2

--- 2023/day08/main/Program.cs
+++ 2023/day08/main/Program.cs
@@ -2,39 +2,37 @@
 
 var input = File.ReadAllText("input.txt");
 
-var result = Day8.SolvePart1(input);
+var part1 = Day8.SolvePart1(input);
 
-Console.WriteLine($"Part 1 solution: {result}");
+Console.WriteLine($"Part 1 solution: {part1}");
+
+var part2 = Day8.SolvePart2(input);
+
+Console.WriteLine($"Part 2 solution: {part2}");
 
 public static class Day8
 {
-    private const string StartingPoint = "AAA";
-
     public static int SolvePart1(string input)
     {
+        var cond = new Func<string, bool>(val => val == "ZZZ");
         var (path, dict) = ParseInput(input);
         var queue = new Queue<char>(path);
 
         var moves = 0;
-        var key = StartingPoint;
-        while (key != "ZZZ")
+        var key = "AAA";
+        while (!cond(key))
         {
             var (left, right) = dict[key];
             moves++;
 
             var move = queue.Dequeue();
 
-            switch (move)
+            key = move switch
             {
-                case 'L':
-                    key = left;
-                    break;
-                case 'R':
-                    key = right;
-                    break;
-                default:
-                    throw new Exception("Invalid input");
-            }
+                'L' => left,
+                'R' => right,
+                _ => throw new Exception("Invalid input")
+            };
 
             queue.Enqueue(move);
         }
@@ -42,18 +40,29 @@ public static class Day8
         return moves;
     }
 
-    private static (string, Dictionary<string, (string, string)>) ParseInput(string input)
+    private static (
+        string,
+        Dictionary<string, (string, string)>
+        ) ParseInput(string input)
     {
         var path = string.Empty;
         var dict = new Dictionary<string, (string, string)>();
-        foreach (var line in input.Split("\n", StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries))
+        foreach (
+            var line in input.Split(
+                "\n",
+                StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries
+            )
+        )
             if (!line.Contains('='))
             {
                 path = line;
             }
             else
             {
-                var eq = line.Split('=', StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries);
+                var eq = line.Split(
+                    '=',
+                    StringSplitOptions.TrimEntries | StringSplitOptions.RemoveEmptyEntries
+                );
 
                 var key = eq[0];
                 var values = new List<string>();
@@ -64,11 +73,59 @@ public static class Day8
                     values.Add(val);
                 }
 
-                if (values.Count() != 2) throw new Exception("Invalid input");
+                if (values.Count != 2)
+                    throw new Exception("Invalid input");
 
                 dict.Add(key, (values[0], values[1]));
             }
 
         return (path, dict);
+    }
+
+    public static long SolvePart2(string input)
+    {
+        var (path, dict) = ParseInput(input);
+
+        var starts = dict.Keys.Where(k => k.EndsWith('A')).ToList();
+        var cycles = new List<long>();
+
+        foreach (var start in starts)
+        {
+            var current = start;
+            long steps = 0;
+            var pathIndex = 0;
+
+            while (!current.EndsWith('Z'))
+            {
+                var move = path[pathIndex];
+                var (left, right) = dict[current];
+
+                current = move == 'L' ? left : right;
+
+                steps++;
+                pathIndex = (pathIndex + 1) % path.Length;
+            }
+
+            cycles.Add(steps);
+        }
+
+        return LcmAll(cycles);
+    }
+
+    private static long Gcd(long a, long b)
+    {
+        while (b != 0)
+            (a, b) = (b, a % b);
+        return a;
+    }
+
+    private static long Lcm(long a, long b)
+    {
+        return a / Gcd(a, b) * b;
+    }
+
+    private static long LcmAll(IEnumerable<long> nums)
+    {
+        return nums.Aggregate(Lcm);
     }
 }
--- 2023/day08/tests/Day8Tests.cs
+++ 2023/day08/tests/Day8Tests.cs
@@ -2,7 +2,7 @@
 
 public class Day8Tests
 {
-    private const string Example =
+    private const string ExamplePart1 =
         """
         RL
 
@@ -15,11 +15,33 @@ public class Day8Tests
         ZZZ = (ZZZ, ZZZ)
         """;
 
+    private const string ExamplePart2 =
+        """
+        LR
+
+        11A = (11B, XXX)
+        11B = (XXX, 11Z)
+        11Z = (11B, XXX)
+        22A = (22B, XXX)
+        22B = (22C, 22C)
+        22C = (22Z, 22Z)
+        22Z = (22B, 22B)
+        XXX = (XXX, XXX)
+        """;
+
     [Fact]
     public void Part1_ShouldBe_2()
     {
-        var result = Day8.SolvePart1(Example);
+        var result = Day8.SolvePart1(ExamplePart1);
 
         Assert.Equal(2, result);
+    }
+
+    [Fact]
+    public void Part2_ShouldBe_6()
+    {
+        var actual = Day8.SolvePart2(ExamplePart2);
+
+        Assert.Equal(6, actual);
     }
 }
