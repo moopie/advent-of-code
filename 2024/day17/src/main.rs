f802bc9f0cda193e786d1fae61ee2318e27242be
Anton Amirian <extuessl@gmail.com>
2026-01-31 19:24:45 +0200

    aoc 2024 day 17 part 2

--- 2024/day17/src/main.rs
+++ 2024/day17/src/main.rs
@@ -1,4 +1,4 @@
-use std::fs::read_to_string;
+use std::{collections::VecDeque, fmt, fs::read_to_string};
 
 struct Cpu {
     a: u64,
@@ -7,6 +7,16 @@ struct Cpu {
     ip: usize,
     program: Vec<u64>,
     output: Vec<u64>,
+}
+
+impl fmt::Display for Cpu {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(
+            f,
+            "ip={:<2} A={:<6} B={:<6} C={:<6} | out={:?}",
+            self.ip, self.a, self.b, self.c, self.output,
+        )
+    }
 }
 
 fn main() {
@@ -23,21 +33,37 @@ fn main() {
     println!("Part 2 solution: {}", part2);
 }
 
-fn solve_part2(input: &str) -> String {
-    "".to_string()
+fn solve_part2(input: &str) -> u64 {
+    let base = parse_input(input);
+    let program = base.program.clone();
+    let n = program.len();
+
+    // queue of (offset, value_for_A)
+    let mut queue: VecDeque<(usize, u64)> = VecDeque::new();
+    queue.push_back((n - 1, 0));
+
+    while let Some((offset, value)) = queue.pop_front() {
+        for digit in 0..8u64 {
+            let new_value = (value << 3) | digit;
+
+            let out = run_with_a(&base, new_value);
+
+            // output must equal the suffix program[offset..]
+            if out == program[offset..] {
+                if offset == 0 {
+                    return new_value; // smallest A found
+                }
+
+                queue.push_back((offset - 1, new_value));
+            }
+        }
+    }
+
+    panic!("No solution found for part 2");
 }
 
 fn solve_part1(input: &str) -> String {
     let mut cpu = parse_input(input);
-
-    let mut cpu = Cpu {
-        a: cpu.a,
-        b: cpu.b,
-        c: cpu.c,
-        ip: 0,
-        program: cpu.program,
-        output: Vec::new(),
-    };
 
     run(&mut cpu);
 
@@ -117,9 +143,23 @@ fn run(cpu: &mut Cpu) {
     }
 }
 
+fn run_with_a(base_cpu: &Cpu, a: u64) -> Vec<u64> {
+    let mut cpu = Cpu {
+        a,
+        b: 0,
+        c: 0,
+        ip: 0,
+        program: base_cpu.program.clone(),
+        output: Vec::new(),
+    };
+
+    run(&mut cpu);
+    cpu.output
+}
+
 #[cfg(test)]
 mod tests {
-    use crate::solve_part1;
+    use super::*;
 
     const EXAMPLE_INPUT: &str = r#"
     Register A: 729
@@ -129,10 +169,24 @@ mod tests {
     Program: 0,1,5,4,3,0
     "#;
 
+    const EXAMPLE_INPUT2: &str = r#"
+    Register A: 2024
+    Register B: 0
+    Register C: 0
+
+    Program: 0,3,5,4,3,0
+    "#;
+
     #[test]
     fn part1_should_be_4_6_3_5_6_3_5_2_1_0() {
         let solution = solve_part1(EXAMPLE_INPUT);
 
         assert_eq!("4,6,3,5,6,3,5,2,1,0", solution);
+    }
+
+    #[test]
+    fn part2_should_be_117440() {
+        let solution = solve_part2(EXAMPLE_INPUT2);
+        assert_eq!(117440, solution);
     }
 }
