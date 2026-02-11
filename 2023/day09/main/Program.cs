1146ee4dfaad8b18f3decd5c04fc2bde2944d5f8
Anton Amirian <extuessl@gmail.com>
2026-02-11 19:34:34 +0200

    aoc 2023 day 9 part 1

---
+++ 2023/day09/main/Program.cs
@@ -0,0 +1,70 @@
+﻿Console.WriteLine("AOC 2023 day 09!");
+
+var input = File.ReadAllText("input.txt");
+
+Console.WriteLine($"Part 1: {Day09.SolvePart1(input)}");
+
+public static class Day09
+{
+    public static long SolvePart1(string input)
+    {
+        var histories = ParseInput(input);
+
+        return histories.Select(ExtrapolateNext).Sum();
+    }
+
+    public static List<List<long>> ParseInput(string input)
+    {
+        return input
+            .Split("\n", StringSplitOptions.RemoveEmptyEntries)
+            .Where(x => !string.IsNullOrEmpty(x))
+            .Select(x => x
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(long.Parse)
+                    .ToList())
+            .ToList();
+    }
+
+    private static List<List<long>> BuildDifferenceLevels(List<long> history)
+    {
+        var levels = new List<List<long>>();
+        var current = new List<long>(history);
+
+        while (true)
+        {
+            levels.Add(current);
+
+            if (current.All(x => x == 0))
+            {
+                break;
+            }
+
+            var next = new List<long>(current.Count - 1);
+
+            for (var i = 0; i < current.Count - 1; i++)
+            {
+                next.Add(current[i + 1] - current[i]);
+            }
+
+            current = next;
+        }
+
+        return levels;
+    }
+
+    private static long ExtrapolateNext(List<long> history)
+    {
+        var levels = BuildDifferenceLevels(history);
+
+        long next = 0;
+
+        for (var i = 0; i < levels.Count - 1 && i >= 0; i++)
+        {
+            var level = levels[i];
+            var last = level[^1];
+            next = last + next;
+        }
+
+        return next;
+    }
+}
---
+++ 2023/day09/tests/Day09Tests.cs
@@ -0,0 +1,17 @@
+﻿namespace tests;
+
+public class Day09Tests
+{
+    const string Example =
+        @"
+        0 3 6 9 12 15
+        1 3 6 10 15 21
+        10 13 16 21 30 45
+        ";
+
+    [Fact]
+    public void Part1_ShouldBe_114()
+    {
+        Assert.Equal(114, Day09.SolvePart1(Example));
+    }
+}
