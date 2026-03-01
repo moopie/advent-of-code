1d946c8b53ed055e0051ef51d8236b706b8719f1
Anton Amirian <extuessl@gmail.com>
2026-03-01 19:02:00 +0200

    aoc 2023 day 19 initial commit

---
+++ 2023/day19/main/Day19.cs
@@ -0,0 +1,9 @@
+namespace main;
+
+public static class Day19
+{
+    public static int SolvePart1(string input)
+    {
+        return 0;
+    }
+}
---
+++ 2023/day19/main/Program.cs
@@ -0,0 +1,7 @@
+﻿using main;
+
+Console.WriteLine("AOC 2023 day 19!");
+
+var input = File.ReadAllText("input.txT");
+
+Console.WriteLine($"Part 1: {Day19.SolvePart1(input)}");
